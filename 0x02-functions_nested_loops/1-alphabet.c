b97415dffebfc0783623da8c6593ff4b136031fd
Roromaremeni <scarafallen48@gmail.com>
2023-07-01 10:43:29 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,13 @@
  */
 
 void print_alphabet(void)
+
 {
 	char letter;
-	for(letter 'a',letter <= 'z'; letter++)
-	_putchar(letter);
 
-	_putchar('\n');
+	for (letter 'a', letter <= 'z'; letter++)
+
+	_putchar (letter);
+
+	_putchar ('\n');
 {
